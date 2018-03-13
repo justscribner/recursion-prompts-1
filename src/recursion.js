7b7d296770882867fb5ab1bc7527cce03c211de8
justscribner <justscribner@gmail.com>
2018-03-13 13:58:16 -0500

    got sum working

--- src/recursion.js
+++ src/recursion.js
@@ -13,12 +13,20 @@ var factorial = function(n) {
   return n * factorial(n - 1);
 };
 
-console.log(factorial(5))
+//console.log(factorial(5))
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return array[0] + sum(array.slice(1));
+ 
 };
+
+//console.log(sum([1,2,3,4,5,6]));
+
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
